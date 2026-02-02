26a8a7233cf6683ac3705074e741fcdd9a5f6f7f
Rizky Indiarto <rizkyian78@gmail.com>
2026-02-02 11:41:04 +0700

    fixing build and faster build

--- checkout-app/vite.config.ts
+++ checkout-app/vite.config.ts
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
-  base: '/checkout/',   // 🔴 THIS IS REQUIRED
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: "/checkout/",
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
